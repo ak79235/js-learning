2dcab5ced7716670c340e9bc4254f60e88ef842d
ak79235 <ak79235@gmail.com>
2023-12-29 02:11:49 +0530

    JS Syntax

---
+++ 2-JS_Syntax/1-Basic_syntax.js
@@ -0,0 +1,14 @@
+console.log("Basic print method in JS");
+// variable declaration
+let c,d,e;
+c=5;
+d=c;
+e=c/d;
+let name= "Prometheus";
+// Example of function
+function sayHello(){
+    let age= 23;
+    console.log("Hello "+name+" your age is "+age);
+    console.log("c="+c,"d="+d,"e="+e);
+}
+sayHello();
---
+++ 2-JS_Syntax/2-Operators.js
@@ -0,0 +1,5 @@
+let x,y,sum;
+x=25;
+y=12;
+sum=x+y;
+console.log("Value of x is "+x, "Value of y is "+y, "Sum is "+sum);
