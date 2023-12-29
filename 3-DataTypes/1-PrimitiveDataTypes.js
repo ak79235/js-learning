8310662a495ae9f20ed173d244c56e83399ef974
ak79235 <ak79235@gmail.com>
2023-12-29 15:14:29 +0530

    Data Types-Primitive and Non-primitive

---
+++ 3-DataTypes/1-PrimitiveDataTypes.js
@@ -0,0 +1,39 @@
+// Numbers
+let num=2;
+let num2=1.3;
+let num3=Infinity; // Infinity is a keyword
+let num4=NaN; // NaN is a keyword
+
+// Strings
+let str="Prometheus in double quotes";
+let str2='Prometheus in single quotes';
+let phrase=`Prometheus in backticks`; // Backticks are used for template literals
+let phrase2=`${str} and ${str2} \nUsing backticks we can embed variables inside string`; // Template literals can be used to concatenate strings
+
+// Booleans
+let bool=true;
+let bool2=false;
+
+// null
+let nullVar=null;
+
+// Symbol
+let sym=Symbol("id"); // Symbols are used to create unique identifiers for objects
+
+// BigInt
+let bigInt=1234567890123456789012345678901234567890n; // BigInts are used to store large numbers
+
+console.log("Primitive Data Types");
+console.log(num);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(str);
+console.log(str2);
+console.log(phrase);
+console.log(phrase2);
+console.log(bool);
+console.log(bool2);
+console.log(nullVar);
+console.log(sym);
+console.log(bigInt);
---
+++ 3-DataTypes/2-NonPrimitiveDataType.js
@@ -0,0 +1,8 @@
+// Non Primitive Data Types
+
+let person = new Object(); // Object is a constructor function
+let perso2 = {}; // Object literal syntax
+
+console.log("Non Primitive Data Types");
+console.log(person);
+console.log(perso2);
